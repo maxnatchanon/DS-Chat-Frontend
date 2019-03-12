2710846d0f98b87202d0b0b14390f5507e23c1c3
Natchanon A <natchanon.347@gmail.com>
2019-03-12 14:36:44 +0700

    Add page template

---
+++ src/ChatPage/ChatPage.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { Button } from 'antd';
+import "antd/dist/antd.css";
+import './ChatPage.css';
+
+class ChatPage extends Component {
+
+    render() {
+        return (
+            <div>
+                CHAT PAGE
+            </div>
+        )
+    }
+
+}
+
+export default ChatPage;
---
+++ src/StartPage/StartPage.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { Button } from 'antd';
+import "antd/dist/antd.css";
+import './StartPage.css';
+
+class StartPage extends Component {
+
+    render() {
+        return (
+            <div>
+                START PAGE
+            </div>
+        )
+    }
+
+}
+
+export default StartPage;
