3ba797fe8f8e1ee9c5aef263ce7cf9a1d6df0001
Natchanon A <natchanon.347@gmail.com>
2019-03-17 02:04:48 +0700

    Add chat sider

--- src/ChatPage/ChatPage.jsx
+++ src/ChatPage/ChatPage.jsx
@@ -1,15 +1,51 @@
-import React, { Component } from "react";
-import { Button } from 'antd';
-import "antd/dist/antd.css";
+import React, { Component } from 'react';
+import { Layout } from 'antd';
+import 'antd/dist/antd.css';
 import './ChatPage.css';
+
+import ChatSider from './Component/ChatSider';
 
 class ChatPage extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            joinedList: ['Group A', 'Group B', 'Group C', 'Group D'],
+            allList: ['Group A', 'Group B', 'Group C', 'Group D', 'Group E', 'Group F', 'Group G'],
+            selected: 'j-0',
+            settingVisible: false,
+        }
+    }
+
+    handlePopOverChange = (visible) => {
+        this.setState({
+            ...this.state,
+            settingVisible: visible,
+        })
+    }
+
+    handleAddGroup = () => {
+        // TODO: Show add group form
+    }
+
+    handleLogOut = () => {
+        // TODO: Log out
+    }
+
     render() {
+        const {Content} = Layout;
         return (
-            <div>
-                CHAT PAGE
-            </div>
+            <Layout className='c-background'>
+                <ChatSider
+                handleLogOut={this.handleLogOut}
+                handleAddGroup={this.handleAddGroup}
+                handlePopOverChange={this.handlePopOverChange}
+                state={this.state}
+                />
+                <Content>
+                    
+                </Content>
+            </Layout>
         )
     }
 
---
+++ src/ChatPage/Component/ChatSider.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { Layout, Menu, Icon, Divider, Popover } from 'antd';
+import "antd/dist/antd.css";
+import './ChatSider.css';
+
+export default class ChatSider extends Component {
+    render() {
+        const {Sider} = Layout;
+        const SubMenu = Menu.SubMenu;
+        return (
+            <Sider className='sider'>
+                <div className='chat-header'>
+                    <Popover
+                    content={<a href='logout' onClick={this.props.handleLogOut}>Log Out</a>}
+                    trigger='click'
+                    placement='bottomLeft'
+                    visible={this.props.state.settingVisible}
+                    onVisibleChange={this.props.handlePopOverChange}
+                    >
+                        <Icon type='setting'/>
+                    </Popover>
+                    <span>Chat</span>
+                    <Icon onClick={this.props.handleAddGroup} type='plus'/>
+                </div>
+                <div className='divider'><Divider/></div>
+                <Menu theme='dark' mode='inline' defaultOpenKeys={['joinedGroup']}>
+                    <SubMenu key='joinedGroup' title={<span><Icon type="check-circle"/>Joined group</span>}>
+                        { this.props.state.joinedList.map((group,idx) => 
+                            <Menu.Item key={'j-'+idx}>{group}</Menu.Item>
+                        ) }
+                    </SubMenu>
+                    <SubMenu key='allGroup' title={<span><Icon type="bars"/>All Group</span>}>
+                        { this.props.state.allList.map((group,idx) => 
+                            <Menu.Item key={'a-'+idx}>{group}</Menu.Item>
+                        ) }
+                    </SubMenu>
+                </Menu>
+            </Sider>
+        )
+    }
+}
