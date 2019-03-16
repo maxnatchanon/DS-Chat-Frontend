e286fc52dd801f88e52fd05e1792643c210f5642
Natchanon A <natchanon.347@gmail.com>
2019-03-16 23:21:35 +0700

    Add log in form

--- src/StartPage/StartPage.jsx
+++ src/StartPage/StartPage.jsx
@@ -1,14 +1,38 @@
 import React, { Component } from "react";
-import { Button } from 'antd';
+import { Button, Input } from 'antd';
 import "antd/dist/antd.css";
 import './StartPage.css';
 
 class StartPage extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+        }
+    }
+
+    handleUsernameChange = (e) => {
+        this.setState({
+            username: e.target.value,
+        });
+        console.log(this.state.username)
+    }
+
+    handleLogIn = () => {
+        // TODO: Log in and go to chat page
+    }
+
     render() {
         return (
-            <div>
-                START PAGE
+            <div className='background center-child'>
+                <div className='login-container'>
+                    <div className='header center-child'>F DS For Sure</div>
+                    <div className='body center-child'>
+                        <Input placeholder='Username' onChange={this.handleUsernameChange} size='large'/>
+                        <Button onClick={this.handleLogIn}>LOG IN</Button>
+                    </div>
+                </div>
             </div>
         )
     }
