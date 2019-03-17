b06077296306d97427faadf19b91b80320326fff
Akamate Chayapiwat <aukmate@gmail.com>
2019-03-18 00:26:29 +0700

    add register function

--- src/StartPage/StartPage.jsx
+++ src/StartPage/StartPage.jsx
@@ -2,9 +2,30 @@ import React, { Component } from "react";
 import { Button } from 'antd';
 import "antd/dist/antd.css";
 import './StartPage.css';
+import axios from 'axios';
+import io from 'socket.io-client';
 
 class StartPage extends Component {
 
+
+     state = {
+        username: "",
+     };
+
+     submit = (event) => {
+        let that = this;
+        axios.post(/*IP*/ + '/register', { name: this.state.username })
+        .then(function (response) {
+        console.log(response);
+        // cookies.set('isAuthen', 'true', { path: '/', maxAge: 60 * 60 * 24 });
+        // cookies.set('username', that.state.username, { path: '/', maxAge: 60 * 60 * 24 });
+        // cookies.set('uid', response.data.uid, { path: '/', maxAge: 60 * 60 * 24 });
+   //     window.location = '/main';
+        })
+        .catch(function (err) {
+           console.error(err);
+        });
+    }
     render() {
         return (
             <div>
